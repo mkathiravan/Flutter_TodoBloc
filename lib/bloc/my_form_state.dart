26263266296f0fe7831dcf620498a7622297f7dc
AshishKumarYadav <hiiashish1@gmail.com>
2022-03-22 23:31:17 +0530

    Initial commit

---
+++ lib/bloc/my_form_state.dart
@@ -0,0 +1,34 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc2/models/email.dart';
+import 'package:flutter_bloc2/models/password.dart';
+import 'package:formz/formz.dart';
+
+class MyFormState extends Equatable
+{
+  const MyFormState({
+    this.email = const Email.pure(),
+    this.password = const Password.pure(),
+    this.status = FormzStatus.pure,
+});
+
+  final Email email;
+  final Password password;
+  final FormzStatus status;
+
+  MyFormState copyWith({
+    Email? email,
+    Password? password,
+    FormzStatus? status,
+  }) {
+    return MyFormState(
+      email: email ?? this.email,
+      password: password ?? this.password,
+      status: status ?? this.status,
+    );
+  }
+
+  @override
+  List<Object> get props => [email, password, status];
+
+
+}
